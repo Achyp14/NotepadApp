c4b5f82e885623a7baca1042a723a56a2da6a892
Andrii A. Chypurko <achypur@softserveinc.com>
2016-06-07 10:19:36 +0300

    megre

--- app/src/main/java/com/example/achypur/notepadapp/Component/ActivityComponent.java
+++ app/src/main/java/com/example/achypur/notepadapp/Component/ActivityComponent.java
@@ -1,6 +1,8 @@
 package com.example.achypur.notepadapp.Component;
 
 
+import android.app.Activity;
+
 import com.example.achypur.notepadapp.Module.ActivityModule;
 import com.example.achypur.notepadapp.PerActivity;
 
@@ -9,15 +11,9 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 
-<<<<<<< HEAD
-@Singleton
-@Component(modules = ActivityModule.class)
-public interface ActivityComponent {
-=======
+
 @PerActivity
 @Component(dependencies = AppComponent.class, modules = ActivityModule.class)
 public interface ActivityComponent {
     Activity activity();
->>>>>>> 3a2fd3db704c1e80b34c80c775e74ed3b8794240
-
 }
